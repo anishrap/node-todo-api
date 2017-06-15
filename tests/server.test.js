f258ff454278c8bb9cc9912e41b758f2a2212e78
Anish Raparla <anishrap@umich.edu>
2017-06-15 16:02:44 +0530

    Updated Server.js

---
+++ tests/server.test.js
@@ -0,0 +1,33 @@
+const expect = require('expect');
+const request = require('supertest');
+var {app} = require('./../server/server.js');
+var {Todo} = require('./../server/models/Todo');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Hello';
+
+
+    request(app)
+      .post('/Todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) { return done(err); }
+
+        Todo.find().then((todos) => {
+          expect(Todos.length).toBe(1);
+          expect(Todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+});
